591e0d3d03f1cd8d4c679ff61f0dcf802552a38f
ArmaanS1ddiqui <armaansiddiqui1704@gmail.com>
2024-08-22 00:18:44 +0530

    V2-August

--- src/hooks/usefetch.js
+++ src/hooks/usefetch.js
@@ -2,17 +2,48 @@ import { useEffect, useState } from "react";
 
 export const useFetch = (apiPath, queryTerm = "") => {
   const [data, setData] = useState([]);
-  const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`;
+  const [pageNum, setPageNum] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchMovies = async (page = 1) => {
+    setIsLoading(true);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}&page=${page}`;
+    const response = await fetch(url);
+    const json = await response.json();
+
+    if (json.results.length > 0) {
+      setData((prevData) => {
+        // Ensure no duplicate movies
+        const uniqueData = [...prevData, ...json.results].reduce(
+          (acc, movie) => {
+            if (!acc.some((item) => item.id === movie.id)) {
+              acc.push(movie);
+            }
+            return acc;
+          },
+          []
+        );
+        return uniqueData;
+      });
+      setPageNum(page + 1);
+      if (json.results.length < 20) {
+        setHasMore(false);
+      }
+    } else {
+      setHasMore(false);
+    }
+
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(url);
+    // Fetch initial data when the component mounts or apiPath/queryTerm changes
+    setData([]);
+    setPageNum(1);
+    setHasMore(true);
+    fetchMovies(1);
+  }, [apiPath, queryTerm]);
 
-      const json = await response.json();
-      setData(json.results);
-    }
-    fetchMovies();
-  }, [url]);
-
-  return { data };
+  return { data, isLoading, hasMore, fetchMore: () => fetchMovies(pageNum) };
 };
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,11 +3,11 @@ import { useFetch } from "../hooks/usefetch";
 import { useEffect } from "react";
 
 export const MovieList = ({ apiPath, title }) => {
-  const { data: movies } = useFetch(apiPath);
+  const { data: movies, isLoading, hasMore, fetchMore } = useFetch(apiPath);
 
   useEffect(() => {
     document.title = `CineVault | ${title}`;
-  });
+  }, [title]);
 
   return (
     <main>
@@ -17,6 +17,17 @@ export const MovieList = ({ apiPath, title }) => {
             <Card key={movie.id} movie={movie} />
           ))}
         </div>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && hasMore && (
+          <div className="flex justify-center mt-5">
+            <button
+              onClick={fetchMore}
+              className="bg-blue-500 text-white py-2 px-4 rounded"
+            >
+              Load More
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
