46db1d1a51b682500e483042386f1384a306d448
ArmaanS1ddiqui <armaansiddiqui1704@gmail.com>
2024-08-22 04:08:02 +0530

    v2.1 August

--- src/hooks/usefetch.js
+++ src/hooks/usefetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 export const useFetch = (apiPath, queryTerm = "") => {
   const [data, setData] = useState([]);
@@ -6,36 +6,39 @@ export const useFetch = (apiPath, queryTerm = "") => {
   const [isLoading, setIsLoading] = useState(false);
   const [hasMore, setHasMore] = useState(true);
 
-  const fetchMovies = async (page = 1) => {
-    setIsLoading(true);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}&page=${page}`;
-    const response = await fetch(url);
-    const json = await response.json();
+  const fetchMovies = useCallback(
+    async (page = 1) => {
+      setIsLoading(true);
+      const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}&page=${page}`;
+      const response = await fetch(url);
+      const json = await response.json();
 
-    if (json.results.length > 0) {
-      setData((prevData) => {
-        // Ensure no duplicate movies
-        const uniqueData = [...prevData, ...json.results].reduce(
-          (acc, movie) => {
-            if (!acc.some((item) => item.id === movie.id)) {
-              acc.push(movie);
-            }
-            return acc;
-          },
-          []
-        );
-        return uniqueData;
-      });
-      setPageNum(page + 1);
-      if (json.results.length < 20) {
+      if (json.results.length > 0) {
+        setData((prevData) => {
+          // Ensure no duplicate movies
+          const uniqueData = [...prevData, ...json.results].reduce(
+            (acc, movie) => {
+              if (!acc.some((item) => item.id === movie.id)) {
+                acc.push(movie);
+              }
+              return acc;
+            },
+            []
+          );
+          return uniqueData;
+        });
+        setPageNum(page + 1);
+        if (json.results.length < 20) {
+          setHasMore(false);
+        }
+      } else {
         setHasMore(false);
       }
-    } else {
-      setHasMore(false);
-    }
 
-    setIsLoading(false);
-  };
+      setIsLoading(false);
+    },
+    [apiPath, queryTerm]
+  ); // Dependencies of useCallback
 
   useEffect(() => {
     // Fetch initial data when the component mounts or apiPath/queryTerm changes
@@ -43,7 +46,7 @@ export const useFetch = (apiPath, queryTerm = "") => {
     setPageNum(1);
     setHasMore(true);
     fetchMovies(1);
-  }, [apiPath, queryTerm]);
+  }, [apiPath, queryTerm, fetchMovies]);
 
   return { data, isLoading, hasMore, fetchMore: () => fetchMovies(pageNum) };
 };
